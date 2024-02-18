96462724d7762d4fb19489f1a761f54b995851fd
LEE\hoho3 <hoho3419@naver.com>
2024-02-19 08:03:11 +0900

    네이버 TV fn 제작

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 (function () {
-  // Checks for ads and manipulates the video or uses skip button if present
-  function handleVideoAd() {
+
+  // 유튜브 Checks for ads and manipulates the video or uses skip button if present
+  function YThandleVideoAd() {
     const video = document.querySelector('video');
     const adElement = document.querySelector('.video-ads.ytp-ad-module');
     // Support for Hulu
@@ -9,13 +10,22 @@
       muteAndSpeedUp(video, 16.0)
     }
     // Skip button seems to be acessible at initialization, if its ever present
-    const skipButton = document.querySelector('.ytp-ad-skip-button, .ytp-ad-skip-button-modern');
+    const skipButton = document.querySelector('.ytp-ad-skip-button-modern.ytp-button');
     if (skipButton) {
       skipButton.click();
-      console.log('Used Button to Skip Ad');
+      console.log('유튜브 광고 스킵 버튼 클릭');
     }
     if (adVideo) {
       muteAndSpeedUp(adVideo, 15.0);
+      // const ad = document.querySelector('.video-stream.html5-main-video'); video-stream html5-main-video
+    }
+  } 
+  // 네이버 TV
+  function NTVhandleVideoAd() {
+    const skipButton = document.querySelector('.btn_skip');
+    if (skipButton) {
+      skipButton.click();
+      console.log('네이버 광고 스킵 버튼 클릭');
     }
   }
 
@@ -25,9 +35,23 @@
   }
 
   function initializeAdHandling() {
-    handleVideoAd();
+    const url = window.location.origin;
+    let fn;
 
-    const observer = new MutationObserver(handleVideoAd);
+    if(url === 'https://www.youtube.com'){ // 유튜브
+      YThandleVideoAd();
+    }
+    else if(url === 'https://tv.naver.com'){ // 네이버 티비
+      NTVhandleVideoAd()
+    }
+    else if(url === 'https://chzzk.naver.com'){ // 치지직 
+
+    }
+    else if(url === 'https://www.afreecatv.com'){ // 아프리카
+
+    }
+
+    const observer = new MutationObserver(YThandleVideoAd);
     observer.observe(document.body, { childList: true, subtree: true });
   }
   // Delay before executing the script (required for Hulu)
